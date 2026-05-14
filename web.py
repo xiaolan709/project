3aba516729e552155cab8ae8949e251b0dc84f33
xiaolan709 <mini19980709@gmail.com>
2026-05-14 21:13:42 +0800

    5/14-2

--- web.py
+++ web.py
@@ -43,7 +43,7 @@ def index():
     link += "<a href=/searchQ>搜尋電影(Firestore查詢)</a><hr>"
     link += "<a href=/road>查詢台中市易肇事路口</a><hr>"
     link += "<a href=/weather>查詢縣市天氣預報</a><hr>"
-    link += "<a href=/webhook>本週新片進DB</a><hr>"
+    link += "<a href=/rate>本週新片進DB</a><hr>"
     return link
 
 @app.route("/webhook", methods=["POST"])
@@ -68,6 +68,68 @@ def webhook():
         info += result
     return make_response(jsonify({"fulfillmentText": info}))
 
+
+@app.route("/rate")
+def rate():
+    #本週新片
+    url = "https://www.atmovies.com.tw/movie/new/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    lastUpdate = sp.find(class_="smaller09").text[5:]
+    print(lastUpdate)
+    print()
+
+    result=sp.select(".filmList")
+
+    for x in result:
+        title = x.find("a").text
+        introduce = x.find("p").text
+
+        movie_id = x.find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw/movie/" + movie_id
+        picture = "https://www.atmovies.com.tw/photo101/" + movie_id + "/pm_" + movie_id + ".jpg"
+
+        r = x.find(class_="runtime").find("img")
+        rate = ""
+        if r != None:
+            rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
+            if rr == "G":
+                rate = "普遍級"
+            elif rr == "P":
+                rate = "保護級"
+            elif rr == "F2":
+                rate = "輔12級"
+            elif rr == "F5":
+                rate = "輔15級"
+            else:
+                rate = "限制級"
+
+        t = x.find(class_="runtime").text
+
+        t1 = t.find("片長")
+        t2 = t.find("分")
+        showLength = t[t1+3:t2]
+
+        t1 = t.find("上映日期")
+        t2 = t.find("上映廳數")
+        showDate = t[t1+5:t2-8]
+
+        doc = {
+            "title": title,
+            "introduce": introduce,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": int(showLength),
+            "rate": rate,
+            "lastUpdate": lastUpdate
+        }
+
+        db = firestore.client()
+        doc_ref = db.collection("本週新片含分級").document(movie_id)
+        doc_ref.set(doc)
+    return "本週新片已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
