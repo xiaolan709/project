42bae03c4a8c761d7b866b7872126ecf9ec1cfa3
xiaolan709 <mini19980709@gmail.com>
2026-05-19 10:08:01 +0800

    5/19

--- web.py
+++ web.py
@@ -44,7 +44,12 @@ def index():
     link += "<a href=/road>查詢台中市易肇事路口</a><hr>"
     link += "<a href=/weather>查詢縣市天氣預報</a><hr>"
     link += "<a href=/rate>本週新片進DB</a><hr>"
+    link += "<a href=/demo>聊天機器人</a><hr>"
     return link
+
+@app.route("/demo")
+def course():
+    return render_template("demo.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
